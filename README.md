3d6e23a14cf544b448d6a96989a60d1b7f9dc4e5
sashpawar11 <saisaipawar@gmail.com>
2022-07-19 11:12:04 +0530

    First

---
+++ README.md
@@ -0,0 +1,11 @@
+## Development
+
+Install Dependencies if you have node installed
+```bash
+    npm i
+```
+
+Run the following command to build the css
+```bash
+    npm run css
+```
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  content: ['./public/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors: {
+
+      'black': '#000000',
+      'blue': '#1fb6ff',
+      'red' : 'rgb(255, 0, 0)',
+      'purple': '#7e5bef',
+      'pink': '#FF0066',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+
+    extend: {},
+  },
+  plugins: [
+    require('./node_modules/tw-elements/dist/plugin')
+  ],
+};
